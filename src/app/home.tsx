3627d581667a80aff099ddc447a114c85b0d58df
eugene huang <eugene@goforward.com>
2023-12-30 17:39:05 -0800

    add homepage with username input

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,8 +1,19 @@
 export function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div>Hello!</div>
-      <button className="btn btn-sm btn-primary mx-4">Button</button>
+    <div className="grid grid-rows-[auto,1fr] h-screen">
+      {/* Nav bar */}
+      <div className="navbar bg-base-100">
+        <a className="btn btn-ghost text-lg">Star Explorer</a>
+      </div>
+      <div className="flex items-center justify-center">
+        {/* Username input */}
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Github Username</span>
+          </div>
+          <input type="text" placeholder="elh" className="input input-sm input-bordered w-full max-w-xs" />
+        </label>
+      </div>
     </div>
   );
 }
